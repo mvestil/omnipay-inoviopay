3fb25ce744968e3d5defceb3e03363d37fa54245
Mark Bonnie Vestil <mark@incube8.sg>
2019-03-26 15:47:10 +0800

    Add space

--- src/Message/Response.php
+++ src/Message/Response.php
@@ -63,7 +63,7 @@ class Response extends AbstractResponse implements RedirectResponseInterface
     public function getMessage()
     {
         if (!empty($this->data['SERVICE_ADVICE']) && !empty($this->data['PROCESSOR_ADVICE'])) {
-            return trim($this->data['SERVICE_ADVICE']) . '(' . trim($this->data['PROCESSOR_ADVICE']) . ')';
+            return trim($this->data['SERVICE_ADVICE']) . ' (' . trim($this->data['PROCESSOR_ADVICE']) . ')';
         }
 
         // InovioPay returns advices with empty spaces, so use trim()
